0f3ce0bdee47e68745713645ff7e1846c898ce7f
Talha <talha.ekrem.99@gmail.com>
2022-08-11 16:55:24 +0300

    get request blog obj changed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  let data = { id: 1, name: "Iphone", price: 29000 };
-  res.send(data);
+  const blog = { id: 1, title: "Blog title", description: "Blog description" };
+  res.send(blog);
 });
 app.listen(port, () => console.log(`Port:${port}. Server is up...`));
