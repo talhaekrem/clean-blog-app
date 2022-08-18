7660f3db92479dc905aab787c696e03cd91bddb7
Talha <talha.ekrem.99@gmail.com>
2022-08-18 17:36:22 +0300

    nodejs work8. ejs kurulumu, partials oluşturuldu.

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
 const app = express();
 const port = 3000;
 
+//TEMPLATE ENGINE
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//ROUTES
 app.get("/", (req, res) => {
-  const blog = { id: 1, title: "Blog title", description: "Blog description" };
-  res.send(blog);
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add", (req, res) => {
+  res.render("add_post");
 });
 app.listen(port, () => console.log(`Port:${port}. Server is up...`));
